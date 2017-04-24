3c796a4aefb930a1087e6cfd8d35bbc47864078f
patw <patw.hi@gmail.com>
2017-04-25 01:28:35 +0800

    Added webpack 2 settings

---
+++ config/jest/setup.js
@@ -0,0 +1,41 @@
+import jsdom from 'jsdom';
+import sinon from 'sinon';
+
+// localStorage
+class LocalStorageMock {
+  constructor() {
+    this.store = {};
+  }
+
+  clear() {
+    this.store = {};
+  }
+
+  getItem(key) {
+    return this.store[key];
+  }
+
+  setItem(key, value) {
+    this.store[key] = value.toString();
+  }
+}
+
+window.localStorage = new LocalStorageMock();
+window.__SERVER__ = false;
+window.__DEVELOPMENT__ = false;
+
+// Define some html to be our basic document
+// JSDOM will consume this and act as if we were in a browser
+const DEFAULT_HTML = '<!doctype html><html><body></body></html>';
+
+// Define some variables to make it look like we're a browser
+// First, use JSDOM's fake DOM as the document
+global.document = jsdom.jsdom(DEFAULT_HTML);
+
+// Set up a mock window
+global.window = document.defaultView;
+
+// Allow for things like window.location
+global.navigator = window.navigator;
+
+global.XMLHttpRequest = sinon.useFakeXMLHttpRequest();
